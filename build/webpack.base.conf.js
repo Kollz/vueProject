c197d2bcccaa89a63cf943c0b534446b166857c0
Yorick Becmeur <yorick.becmeur@autoliv.com>
2017-05-29 14:52:06 +0200

    correction config

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -53,7 +53,7 @@ module.exports = {
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'file',
-        include: './assets/img'
+        include: './assets/img',
         query: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
